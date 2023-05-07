839ddd53328a3bf359df0694b25d4073a07a4f10
destanuarisufiaa <destanuarisufia25@gmail.com>
2023-05-07 19:42:16 +0700

    RINGTONE PESANAN BERHASIL (PERSIAPAN, ANTAR, SELESAI)

--- app/src/main/java/com/compare/compare_user/MainActivity.kt
+++ app/src/main/java/com/compare/compare_user/MainActivity.kt
@@ -1,15 +1,23 @@
 package com.compare.compare_user
 
+import android.content.ContentValues.TAG
 import android.content.Context
 import android.content.Intent
+import android.media.Ringtone
+import android.media.RingtoneManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.compare.compare_user.databinding.ActivityMainBinding
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding : ActivityMainBinding
-
+    private lateinit var ringtone: Ringtone
+    private var isNotificationPlayed = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,6 +44,62 @@ class MainActivity : AppCompatActivity() {
             }
 
             true
+        }
+        // Mengambil referensi dari Firestore
+        val db = Firebase.firestore
+        val pesananRef = db.collection("pesanan")
+
+        // Mendefinisikan uri dari file audio
+        val ringtonePersiapanUri = Uri.parse("android.resource://com.compare.compare_user/" + R.raw.disiapkan)
+        val ringtoneAntarUri = Uri.parse("android.resource://com.compare.compare_user/" + R.raw.diantar)
+        val ringtoneSelesaiUri = Uri.parse("android.resource://com.compare.compare_user/" + R.raw.selesai)
+
+        // Menambahkan listener untuk mengambil data
+        pesananRef.addSnapshotListener { snapshot, e ->
+            if (e != null) {
+                Log.w(TAG, "Listen failed.", e)
+                return@addSnapshotListener
+            }
+
+            // Memastikan bahwa ada data yang ditemukan
+            if (snapshot != null && !snapshot.isEmpty) {
+                for (doc in snapshot.documents) {
+                    val statusPesanan = doc.getString("status")
+
+                    // Memeriksa status pesanan dan memainkan ringtone yang sesuai
+                    when (statusPesanan) {
+                        "PERSIAPAN" -> {
+                            if (!isNotificationPlayed) {
+                                ringtone = RingtoneManager.getRingtone(applicationContext, ringtonePersiapanUri)
+                                ringtone.play()
+                                isNotificationPlayed = true
+                            }
+                        }
+                        "ANTAR" -> {
+                            if (!isNotificationPlayed) {
+                                ringtone = RingtoneManager.getRingtone(applicationContext, ringtoneAntarUri)
+                                ringtone.play()
+                                isNotificationPlayed = true
+                            }
+                        }
+                        "SELESAI" -> {
+                            if (!isNotificationPlayed) {
+                                ringtone = RingtoneManager.getRingtone(applicationContext, ringtoneSelesaiUri)
+                                ringtone.play()
+                                isNotificationPlayed = true
+                            }
+                        }
+                        else -> {
+                            if (isNotificationPlayed) {
+                                ringtone.stop()
+                                isNotificationPlayed = false
+                            }
+                        }
+                    }
+                }
+            } else {
+                Log.d(TAG, "Current data: null")
+            }
         }
     }
 
